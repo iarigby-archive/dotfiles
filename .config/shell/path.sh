b29c66b3f5b90c7c9df542ed255e2b5e22763e8d
iarigby <marvinzem@gmail.com>
2020-05-07 18:07:07 +0400

    redundant path

--- .config/shell/path.sh
+++ .config/shell/path.sh
@@ -1,6 +1,5 @@
 PATH=$PATH:/home/$USER/.local/bin
 PATH=/home/$USER/scripts:$PATH
-PATH=/home/$USER/dev/scripts:$PATH
 # PATH=$PATH:$(ruby -e 'puts Gem.user_dir')/bin
 PATH=$PATH:/home/$USER/.gem/ruby/2.7.0/bin
 # PATH=$PATH:$(yarn global bin)
