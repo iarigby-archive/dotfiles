51be32b406755d00d7041218c5ef6e6562a8fd54
ia rigby <marvinzem@gmail.com>
2022-01-22 20:04:48 +0400

    hub stuff

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -6,10 +6,11 @@ alias more=less
 alias ssdate="date '+%Y-%m-%d_%H:%M:%S'"
 alias sdate="date '+%Y-%m-%d_%H:%M'"
 
-alias vim=nvim
 alias ws=webstorm
 alias sl=subl3
 alias o=xdg-open
 alias dt=~/dev/freeuni/subject-modules
+fdt='freeuni-digital-technologies'
+twi='theworksinstitute'
 
 
